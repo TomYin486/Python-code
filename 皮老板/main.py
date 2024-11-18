40fe35ee0193bf328969cbed6709c062a0cfa650
jojo--1 <1468608544@qq.com>
2024-11-18 21:34:40 +0800

    python：给定两个字符串 s 和 goal。如果在若干次旋转操作之后，s 能变成 goal，那么返 回 true，否则返回 False

--- 皮老板/main.py
+++ 皮老板/main.py
@@ -1,12 +1,19 @@
-# 该函数接受两个参数：words（列表）和 s（字符串）
-def countPrefixes(words:list,s:str):
-    count = 0
-    # 遍历 words 列表中的每个字符串 word
-    for word in words:
-        # 使用 startswith 方法检查字符串 s 是否以 word 作为前缀开始
-        # 如果 word 是 s 的前缀，startswith 方法返回 True；如果不是，则返回 False
-        if s.startswith(word):
-            count += 1
-    return count
+def rotateString(s,goal):
+    # 检查字符串 s 和 goal 的长度是否相等。如果长度不同，那么 s 不可能通过旋转变成 goal
+    if len(s) != len(goal):
+        return False
+    else:
+        # 这个 (s+s) 包含了 s 的两次重复，从而覆盖了 s 的所有可能的旋转
+        # 如果 goal 是 s 的一个旋转，那么它也是 (s+s) 的子串
+        if goal in (s+s):
+            return True
+        else:
+            return False
 
-print(countPrefixes(['a','abc','aa'],'abc'))
+# 可以简化
+# def rotateString(s,goal):
+#     if len(s) != len(goal):
+#         return False
+#     return goal in (s+s)
+
+print(rotateString('cab','abc'))
