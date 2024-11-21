f426c916812ff1ac5ddf085a8d6f7666aca2bc31
jojo--1 <1468608544@qq.com>
2024-11-21 18:23:56 +0800

    python：在指定的目录及其子目录中搜索包含特定关键字的文件，并打印出这些文件的路径 （输入“待搜索的路径”和“关键字”，使用 os.walk()方法）

--- 皮老板/main.py
+++ 皮老板/main.py
@@ -1,19 +1,18 @@
-def rotateString(s,goal):
-    # 检查字符串 s 和 goal 的长度是否相等。如果长度不同，那么 s 不可能通过旋转变成 goal
-    if len(s) != len(goal):
-        return False
-    else:
-        # 这个 (s+s) 包含了 s 的两次重复，从而覆盖了 s 的所有可能的旋转
-        # 如果 goal 是 s 的一个旋转，那么它也是 (s+s) 的子串
-        if goal in (s+s):
-            return True
-        else:
-            return False
+# 导入了 Python 的 os 模块，该模块提供了对文件和目录的操作的功能
+import os
+inputPath = input('请输入待搜索的路径：')
+pattern = input('请输入关键字：')
 
-# 可以简化
-# def rotateString(s,goal):
-#     if len(s) != len(goal):
-#         return False
-#     return goal in (s+s)
-
-print(rotateString('cab','abc'))
+'''
+使用 os.walk() 函数来遍历 inputPath 指定的目录及其所有子目录
+os.walk() 生成一个三元组 (dirpath, dirnames, filenames)
+其中 dirpath 是当前遍历到的目录路径，dirnames 是该目录下所有子目录的列表，filenames 是该目录下所有非目录文件的列表
+'''
+for dirpath,dirnames,filenames in os.walk(inputPath):
+    # 遍历当前目录下的所有文件名
+    for f in filenames:
+        # 检查当前文件名 f 是否包含之前输入的关键字 pattern
+        # 如果文件名中包含这个关键字，那么条件判断为真
+        if pattern in f:
+            # 打印出包含关键字的文件的完整路径
+            print(f'{inputPath}/{f}')
