11ad2fffe1db61cd128fb86e4717385d1976cd5b
jojo--1 <1468608544@qq.com>
2024-11-28 23:18:32 +0800

    python：处理 Excel 电子表格中的数据，计算对应班级学生的平均分(使用第三方库 xlrd)

--- 皮老板/main.py
+++ 皮老板/main.py
@@ -1,18 +1,36 @@
-def getFrequency(text:str):
-    # 创建了一个空的字典 fre，用于存储每个字母的出现频率
-    fre = {}
+# 导入 xlrd 模块，用于读取Excel文件（.xls和.xlsx）的 Python 库
+import xlrd
 
-    # 使用 for 循环，遍历 text 字符串中的每个字符
-    # 计数时不区分大小写, 使用 text.lower() 将所有的字符都被转换为小写，
-    for c in text.lower():
-        # 检查当前字符 c 是否是在 'a' 到 'z' 之间
-        if 'a'<= c <='z':
-            # 如果字符 c 已经在 fre 字典中，次数加 1
-            if c in fre:
-                fre[c] += 1
-            # 如果字符 c 不在 fre 字典中，这意味着它是第一次出现，设置初始值为 1
-            else:
-                fre[c] = 1
-    return fre
+'''
+这个是 Excel 表中的数据
+   0       1        2
+0  姓名	   班级	    成绩
+1  Alan1   202311	61
+2  Alan2   202311	78
+3  Alan3   202311	89
+4  Alan4   202311	99
+5  Alan5   202311	82
+6  Alan6   202312	66
+'''
 
-print(getFrequency('Tom likes to eat apples'))
+# 1. 打开 xlsx 文件(使用 xlrd.open_workbook 函数打开位于指定路径的Excel文件)
+xlsx = xlrd.open_workbook('d:/浏览器下载的/Jack/excell.xlsx')
+
+# 2. 获取 0 号标签页. (当前只有一个标签页,即通过索引（0号，即第一个标签页）获取Excel文件中的工作表)
+table = xlsx.sheet_by_index(0)
+
+# 3. 获取总行数
+nrows = table.nrows
+
+# 4. 遍历数据
+count = 0
+total = 0
+for i in range(1, nrows):
+    # 使用 cell_value(row, col) 获取到指定坐标单元格的值
+    # 使用 cell_value 方法获取当前行第 1 列的值(列数从 0 开始)
+    classId = table.cell_value(i, 1)
+    if classId == 202311:
+        # 如果班级ID匹配，将当前行第 2 列(列数从 0 开始)的值加到 total 上
+        total += table.cell_value(i, 2)
+        count += 1
+print(f'平均分: {total / count}')
